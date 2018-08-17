e38664cf82b1f0f9d08d64045eb2106b58a2b1a1
Oleksandr Zinenko <git@ozinenko.com>
2018-08-17 23:11:17 +0200

    stupid hardcoded example operator with TC

--- tensorflow/core/user_ops/tc_op.cc
+++ tensorflow/core/user_ops/tc_op.cc
@@ -2,13 +2,17 @@
 #include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/framework/op_kernel.h"
 
-#include <tc/core/tensor.h>
+#include <tc/core/cpu/cpu_tc_executor.h>
+#include "tc/core/polyhedral/scop.h"
+#include "tc/core/polyhedral/codegen_llvm.h"
+#include "tc/core/polyhedral/cpu/mapped_scop.h"
+#include "tc/core/polyhedral/llvm_jit.h"
 
 using namespace tensorflow;
 
 REGISTER_OP("ZeroOut")
-    .Input("to_zero: int32")
-    .Output("zeroed: int32")
+    .Input("to_zero: float")
+    .Output("zeroed: float")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
       c->set_output(0, c->input(0));
       return Status::OK();
@@ -19,24 +23,36 @@ class ZeroOutOp : public OpKernel {
   explicit ZeroOutOp(OpKernelConstruction* context) : OpKernel(context) {}
 
   void Compute(OpKernelContext* context) override {
+    using namespace tc;
+    using namespace tc::polyhedral;
+
     // Grab the input tensor
     const Tensor& input_tensor = context->input(0);
-    auto input = input_tensor.flat<int32>();
+    auto input = input_tensor.flat<float>();
 
     // Create an output tensor
     Tensor* output_tensor = NULL;
     OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
                                                      &output_tensor));
-    auto output_flat = output_tensor->flat<int32>();
+    auto output_flat = output_tensor->flat<float>();
 
-    // Set all but the first element of the output tensor to 0.
+    // Call to TC
+  string tc = R"TC(
+def fun(float(N) A) -> (C) {
+    C(n) = n == 0 ? A(n) : 0.0
+}
+)TC";
+    auto ctx = isl::with_exceptions::globalIslCtx();
+    auto scop = polyhedral::Scop::makeScop(ctx, tc);
     const int N = input.size();
-    for (int i = 1; i < N; i++) {
-      output_flat(i) = 0;
-    }
+    scop = Scop::makeSpecializedScop<int>(*scop, {{"N", N}});
+    auto mscop = cpu::MappedScop::makeSequential(
+        std::move(scop), CpuMappingOptions::makeNaiveMappingOptions());
+    auto pJit = mscop->codegen("kernel_anon");
+    auto fptr = reinterpret_cast<void (*)(float*, float*, int)>(
+        pJit->getSymbolAddress("kernel_anon"));
 
-    // Preserve the first input value if possible.
-    if (N > 0) output_flat(0) = input(0);
+    fptr((float *)input.data(), (float *)output_flat.data(), N);
   }
 };
 
